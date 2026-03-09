a83a667a12ae884faf2b6f051f6898bd5dafa89a
dalymoustafa <dalymoustafa@gmail.com>
2026-03-09 13:26:12 +0200

    Update fetch-offices.js

--- fetch-offices.js
+++ fetch-offices.js
@@ -169,7 +169,7 @@ async function main() {
         });
       });
 
-      map.setView([20, 10], 2);
+      if (OFFICES.length > 0) { map.fitBounds(OFFICES.map(o => [o.lat, o.lng]), { padding: [30, 30], maxZoom: 4 }); } else { map.setView([20, 10], 2); }
     });
   map.setView([20, 10], 2);
 <\/script>
